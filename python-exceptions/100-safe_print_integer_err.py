ce52cb6fd0f56b5a74a01d734c8259ed73df52fd
HSHKZ <lucasolegniel@gmail.com>
2024-09-10 12:20:15 +0200

    function that prints an integer.

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except stderr:
+        print("Exception: {:d}".format(stderr))
