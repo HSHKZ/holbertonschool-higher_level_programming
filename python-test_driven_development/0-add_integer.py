63dfa6ed72fbe0e3deaeb5968439a6002743a558
Lucas Niel <lucasolegniel@gmail.com>
2024-09-11 19:21:21 +0200

    Delete python-test_driven_development directory

--- python-test_driven_development/0-add_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def add_integer(a, b=98):
-    """Adds two integers or floats, casting them to integers if necessary."""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
