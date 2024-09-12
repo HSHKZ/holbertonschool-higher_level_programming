f69a825355b2868442bb3521aa4a3842ec67e8af
HSHKZ <lucasolegniel@gmail.com>
2024-09-12 16:05:22 +0200

    moduf with pycode

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     for i in range(1, 3):  # Loop over the range(1, 3)
         try:
             if i > a:  # Check if i is greater than a
-                raise Exception('Too far')  # Raise an exception if condition is met
+                raise Exception('Too far')
             result += (a ** b) / i  # Perform the calculation and update result
         except Exception:
             result = b + a  # If an exception occurs, update result to b + a
