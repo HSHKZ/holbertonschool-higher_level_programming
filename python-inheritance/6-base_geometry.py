9c3a0e0d06314d01715ef15bdda30adf01f96351
HSHKZ <lucasolegniel@gmail.com>
2024-09-23 12:02:13 +0200

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''6. Improve Geometry'''
+
+
+class BaseGeometry:
+    '''class BaseGeometry (based on 5-base_geometry.py)'''
+
+    def area(self):
+        raise Exception("area() is not implemented")
