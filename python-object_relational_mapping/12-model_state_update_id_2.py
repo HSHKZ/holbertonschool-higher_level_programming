68533b6e0895baedf176a62973f2254d7940619f
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:07:20 +0200

    script that changes the name of a State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-# Changes the name of the State object with id = 2 to
-# New Mexico in the database hbtn_0e_6_usa.
-# Usage: ./12-model_state_update_id_2.py <mysql username> /
-#                                        <mysql password> /
-#                                        <database name>
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
 import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(id=2).first()
+    state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
+    session.add(state)
+
     session.commit()
+    session.close()
