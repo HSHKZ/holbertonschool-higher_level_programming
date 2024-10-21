10e80d5b4a63bfb52f7118fd8ba492df87dd65b7
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:37:00 +0200

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
-
+"""
+Module that display all values in the states table
+of a database where name matches the argument.
+Safe from MySQL injections :)
+"""
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        database=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name \
-                = % s ORDER BY states.id ASC""", (sys.argv[4],))
+    cursor = database.cursor()
 
-    rows = cur.fetchall()
-    for row in rows:
+    cursor.execute("""
+                   SELECT * FROM states
+                   WHERE name LIKE BINARY %s
+                   ORDER BY states.id
+                   """, (sys.argv[4],))
+
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
-    db.close()
+    cursor.close()
+    database.close()
