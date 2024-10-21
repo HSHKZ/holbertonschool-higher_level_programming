570ca2d046b9cac74b1e842fcca46ed35c062faf
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:54:46 +0200

    Python file similar to model_state.py named model_city.py that contains the class definition of a City.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
+"""
+Module that prints all City objects from the database `hbtn_0e_14_usa`.
+"""
 
-
-import MySQLdb
-import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State)
-    cities = cities.filter(City.state_id == State.id)
-    cities = cities.order_by(City.id)
+    cities = (
+        session.query(City, State)
+        .filter(City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
 
     for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.commit
 
-    session.commit()
     session.close()
