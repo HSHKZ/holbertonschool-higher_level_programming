3ba7a986160d39515bc86f2f2947b8a906cc28f9
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:51:16 +0200

    script that changes the name of a State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
+"""
+Module that  that changes the name of a State object from a database.
+"""
 
-
-import MySQLdb
-import sys
-from model_state import Base, State
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = "New Mexico"
-    session.add(state)
+    state_update = session.query(State).filter(State.id == 2).first()
 
-    session.commit()
+    if state_update:
+        state_update.name = "New Mexico"
+        session.commit()
+
     session.close()
