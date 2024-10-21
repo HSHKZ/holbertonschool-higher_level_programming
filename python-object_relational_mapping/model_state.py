1acc0d17c7040309a159c8a20f3aa34c75238799
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:56:39 +0200

    script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Module is documented """
+"""
+Module that defines the `State` class and initializes a `Base` object.
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,8 +10,15 @@ Base = declarative_base()
 
 
 class State(Base):
-    """ Class of state"""
-    __tablename__ = 'states'
+    """
+    Represents a state for a MySQL database.
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(256), nullable=False)
+    Attributes:
+        id (int): an auto-generated, unique integer
+                  representing the state's ID.
+        name (str): a string with a maximum of
+                    128 characters representing the state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
