9eb273b2971fb366cb00b4ebfa6f1f3e7996fddc
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:09:16 +0200

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
-
+"""
+Module that list all states from a database.
+"""
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
+
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        database=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor = database.cursor()
 
-    rows = cur.fetchall()
-    for row in rows:
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
-    db.close()
+    cursor.close()
+    database.close()
