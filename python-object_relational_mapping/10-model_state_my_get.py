22f2a3263720afee47f542de894f8e6d1f079a6e
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:45:32 +0200

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
+"""
+Module that lists all State objects that contain the letter a from a database.
+"""
 
-
-import MySQLdb
-import sys
-from model_state import Base, State
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_name = State.name.like(sys.argv[4])
-    state = session.query(State).filter(state_name).first()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
 
     if state:
-        print(f'{state.id}')
+        print(state.id)
     else:
-        print('Not found')
+        print("Not found")
 
-    session.commit()
     session.close()
