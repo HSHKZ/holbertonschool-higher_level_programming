fde0c152e11c6e73eb148d1b80398f27e30aa246
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 16:39:13 +0200

    script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
-
+"""
+Module that list all cities from a database.
+"""
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(
-        host='localhost',
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        database=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-    INNER JOIN states ON cities.state_id = states.id")
+    cursor = database.cursor()
 
-    rows = cur.fetchall()
-    for row in rows:
+    cursor.execute("""
+                   SELECT cities.id, cities.name, states.name
+                   FROM cities JOIN states
+                   ON cities.state_id = states.id
+                   ORDER BY cities.id
+                   """)
+
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
-    db.close()
+    cursor.close()
+    database.close()
