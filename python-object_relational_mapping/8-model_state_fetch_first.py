a405cf3a9448738adad35013b5c03a20ad1febd5
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 15:53:56 +0200

    script that prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
 import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    state = session.query(State).first()
+
+    if state:
+        print(f'{state.id}: {state.name}')
     else:
-        print("{}: {}".format(state.id, state.name))
+        print('Nothing')
+
+    session.commit()
+    session.close()
