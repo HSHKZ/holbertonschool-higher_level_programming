4b5ca5f30dbcc1e365620a46794d7ea9e7ef6dbc
Lucas Niel <lucasolegniel@gmail.com>
2024-10-21 15:43:40 +0200

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with 'N'
-from the database hbtn_0e_0_usa, sorted by id.
-"""
+"""All states with a name starting with N"""
+
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        try:
-            # Connexion à la base de données
-            db = MySQLdb.connect(
-                host="localhost",
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3],
-                port=3306
-            )
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
-            # Création d'un curseur pour exécuter les requêtes
-            cur = db.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+        WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
-            # Requête SQL pour lister les états dont le nom commence par 'N'
-            cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-            
-            # Récupération des résultats
-            rows = cur.fetchall()
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-            # Affichage des résultats
-            for row in rows:
-                print(row)
-
-            # Fermeture du curseur et de la connexion à la base de données
-            cur.close()
-            db.close()
-
-        except MySQLdb.Error as err:
-            print(f"Error: {err}")
-    else:
-        print("Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>")
+    cur.close()
+    db.close()
