8a6ef9648673a96f89713ba189eea203cf76aeff
HSHKZ <lucasolegniel@gmail.com>
2024-10-31 14:00:58 +0100

    script that prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     database_name = argv[3]
 
     # Create an engine to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
         f'{database_name}'
         )
 
