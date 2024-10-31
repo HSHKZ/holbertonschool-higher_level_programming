1317748253da8f88d9713d65d79cab94d5bfc7b2
HSHKZ <lucasolegniel@gmail.com>
2024-10-31 11:38:42 +0100

    python file that contains the class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,13 +6,18 @@ import sys
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class that links to the MySQL table states."""
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
